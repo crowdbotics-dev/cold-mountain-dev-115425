f962480f007bf0888305ed801f1c19d97bab3d25
Crowdbotics <engineering@crowdbotics.com>
2023-09-10 23:10:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-mountain-dev-115425",
+  url: "https://cold-mountain-dev-115425.botics.co",
+  api: "https://cold-mountain-dev-115425.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
